d12cafb5a3ca81f4eea78a69cf74192ed571ac8a
Deji1127 <aahmedibrahim2002@gmail.com>
2023-03-25 21:31:43 -0500

    .

--- deexnuts/src/App.js
+++ deexnuts/src/App.js
@@ -1,24 +1,66 @@
-import './App.css';
-import Info from "./Info.js";
+import "./App.css";
+import SearchBar from "./SearchBar";
+//import Info from "./Info.js";
+//import { useState } from "react";
+
+
 function App() {
   return (
     <div className="App">
-      <Info title="This is the title"/>
-      <AddItem text="Ahmed" number={7}/>
-      <AddItem text="Saja"/>
+    <SearchBar />
     </div>
-  );
+  )
 }
+//function App() {
+  // return (
+  //   <div className="App">
+  //     <Info title="This is the title" />
+  //     <ButtonState />
+  //     {/* <AddItem text="Ahmed" number={7}/>
+  //     <AddItem text="Saja"/> */}
+  //   </div>
+  // );
+//}
+// function AddItem(props){
+//   const value = props.text;
+//   return(
+//     <form>
+//       <label for="text-form"> Type something: </label>
+//       <input type="text" value={props.text} id="text-form" />
+//       <p>{props.number}</p>
+//     </form>
+//   );
+// }
+// function ButtonState() {
+//   //this is the button function
+//   const [title, setTitle] = useState("");
+//   const [count, setCount] = useState(0);
 
+//   const updateTitleClicked = () => {
+//     setTitle("We got titlesss"); //function passed into onClick //shows a new title
+//   };
+//   const updateCounterClicked = () => {
+//     setCount(count + 1);
+//   };
+//   return (
+//     <div>
+//       {/* <p>Title: {title}</p>
+//       <p>Counter: {count}</p> */}
+//       <Data title={ title } count={count}/>
+//       <button onClick={updateTitleClicked}>About me </button>{/*onClick is responsible for what happens after the button is clicked*/}
+//       <button onClick={updateCounterClicked}>Links and Socials</button>
+//       {/*requires a function to be passed. Function determines what happens*/}
+//     </div>
+//   );
+// }
 
-function AddItem(props){
-  const value = props.text;
-  return( 
-    <form>
-      <label for="text-form"> Type something: </label>
-      <input type="text" value={props.text} id="text-form" />
-      <p>{props.number}</p>
-    </form>
-  );
-}
+// function Data(props) {
+//   return (
+//     <div>
+//       <p>Title: {props.title}</p>
+//       <p>Count: {props.count}</p>
+//     </div>
+//   );
+// }
+
 export default App;
--- deexnuts/src/Info.js
+++ deexnuts/src/Info.js
@@ -1,57 +1,50 @@
-import React from "react";
-class Info extends React.Component{
-    constructor(props){
-        super(props);
-        console.log(props);
-    }
-
-    render(){
-    //const title = "This is the title";
-    const title = this.props.title;
-    const showTitle = true;
-    // return(
-    //   <div>
-    //     <h1>{title}</h1>
-    //     {/* <h1>{showTitle ? title : "No Title"}</h1> */}
-    //     <p>Manage your stuff.</p>
-    //   </div>
-    // );
-    if(showTitle){
-      return(
-        <div>
-          <h1>{title}</h1>
-          {/* <h1>{showTitle ? title : "No Title"}</h1> */}
-          <p>Manage your stuff.</p>
-        </div>
-      );
-    }
-    else{
-      return <p>empty</p>;
-    }
-    }
-}
-// function Info(){
-//     const title = "This is the title";
-//     const showTitle = true;
-//     // return(
-//     //   <div>
-//     //     <h1>{title}</h1>
-//     //     {/* <h1>{showTitle ? title : "No Title"}</h1> */}
-//     //     <p>Manage your stuff.</p>
-//     //   </div>
-//     // );
-//     if(showTitle){
-//       return(
-//         <div>
-//           <h1>{title}</h1>
-//           {/* <h1>{showTitle ? title : "No Title"}</h1> */}
-//           <p>Manage your stuff.</p>
-//         </div>
-//       );
+// import React from "react";
+// class Info extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     {
+//       /*console.log(props);*/
 //     }
-//     else{
-//       return <p>empty</p>;
-//     }
+//     this.state = {
+//       count: 0,
+//     };
 //   }
+// buttonPress(){
+//   this.setState({
+//     count: this.state.count + 1,
+//   });
+// }
+//   render() {
+//     return (
+//       <div>
+//         <p>Count: {this.state.count}</p>
+//         <button onClick={ ()=> this.buttonPress()}>Click me!</button>
+//       </div>
+//     );
+//   }
+// }
+// // function Info(){
+// //     const title = "This is the title";
+// //     const showTitle = true;
+// //     // return(
+// //     //   <div>
+// //     //     <h1>{title}</h1>
+// //     //     {/* <h1>{showTitle ? title : "No Title"}</h1> */}
+// //     //     <p>Manage your stuff.</p>
+// //     //   </div>
+// //     // );
+// //     if(showTitle){
+// //       return(
+// //         <div>
+// //           <h1>{title}</h1>
+// //           {/* <h1>{showTitle ? title : "No Title"}</h1> */}
+// //           <p>Manage your stuff.</p>
+// //         </div>
+// //       );
+// //     }
+// //     else{
+// //       return <p>empty</p>;
+// //     }
+// //   }
 
-export default Info; 
+// export default Info;
