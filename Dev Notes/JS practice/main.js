7258f2636f5618541f7c13370219bc60652d112d
Ibrahim, Ahmed <Ahmed.Ibrahim@fmr.com>
2023-06-22 15:56:18 -0500

    .

---
+++ Dev Notes/JS practice/main.js
@@ -0,0 +1,33 @@
+// // variabled: var, let, const
+//use const when u know u wont reassign values
+// let age = 30;// can reassign values with let
+// age  = 31;
+// console.log(age);
+
+//strings, numbers, boolean, null. undefined
+
+// const name = "john"; //string
+// const age = 8.2; //number
+// const isAwesome = true; //boolean
+// const x = null; //null
+// let c; //undefined
+
+// console.log(typeof x);
+
+// const name = "john"; //string
+// const age = 8.2; //number
+
+
+// let message = `Hi, my name is ${name} and I'm ${age} years old.`;//template string
+// console.log(message)
+// message = "Hi, my name is " + name + " and I'm " + age + " years old."; //concatenation
+// console.log(message)
+
+let s = 'Hello World';
+console.log(s.length)
+
+// Arrays - variables that hold multiple values
+
+const numbers = new Array(1,2,3,4,5);
+console.log(numbers);
+const fruits = ['apples', 'oranges']
